2419056395f174f000985a923bf1be5587e58674
Satoru Fujiomri <satoru.fujimori@gmail.com>
2022-03-09 13:07:38 +0900

    add project

---
+++ ASPNetCoreDownloadSample/Controllers/DownloadController.cs
@@ -0,0 +1,59 @@
+﻿using System.IO.Compression;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreDownloadSample.Controllers
+{
+    public class DownloadController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            // Azure Storage にあるZIPファイル
+            var zipFileName = "XamarinMacAppPipelineTest.app.zip";
+            // ZIP ファイル内の Mac アプリファイル名
+            var sourceAppFileName = "XamarinMacAppPipelineTest.app";
+            // 変更後の Mac アプリファイル名
+            var newAppFileName = "XamarinMacAppPipelineTest-1234567890.app";
+
+            // Azure Storage から ZIP ファイルを取得する
+            var connectionString = Settings.Instance.StorageConnectionString;
+            var blobContainer = new BlobContainerClient(connectionString, "uploads");
+            var blobClient = blobContainer.GetBlobClient(zipFileName);
+            var result = await blobClient.DownloadStreamingAsync();
+
+            // 取得したZIPファイルの Stream から名前を変更した ZIP を取得する
+            var bytes = ChangeZipFolderName(result.Value.Content, sourceAppFileName,newAppFileName);
+
+            // ZIP ファイルとしてダウンロードできる形で返す
+            return this.File(bytes, "application/octet-stream", zipFileName);
+        }
+
+        byte[] ChangeZipFolderName(Stream sourceStream, string sourceAppFileName, string newAppFileName)
+        {
+            // ZIP ファイルをオープンする
+            using var sourceArchive = new ZipArchive(sourceStream, ZipArchiveMode.Read);
+
+            // 新しいZIPを作成する
+            using var memoryStream = new MemoryStream();
+            using var newArchive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true);
+            foreach (var entry in sourceArchive.Entries)
+            {
+                // ディレクトリ名を新しいアプリ名に変更する
+                // var newEntryName = entry.FullName.Replace(sourceAppFileName, newAppFileName);
+                var newEntryName = entry.FullName;
+
+                var newEntry = newArchive.CreateEntry(newEntryName);
+                Console.WriteLine($"Entry:{newEntryName}");
+
+                using (var archiveEntry = entry.Open())
+                using (var newArchiveEntry = newEntry.Open())
+                {
+                    archiveEntry.CopyTo(newArchiveEntry);
+                }
+            }
+            newArchive.Dispose();  // これがないと ZIP の最後部分が書き込まれない
+
+            return memoryStream.ToArray();
+        }
+    }
+}
---
+++ ASPNetCoreDownloadSample/Models/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+public class Settings
+{
+    public Settings()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false)
+            .AddEnvironmentVariables();
+        _instance = builder.Build();
+    }
+
+    public static Settings Instance { get; } = new Settings();
+
+    private readonly IConfigurationRoot _instance;
+
+    public string StorageConnectionString => _instance.GetConnectionString(nameof(StorageConnectionString));
+}
